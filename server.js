1da96e880a9e4481c470f7bb0c7e6c485274a9be
halil ozer <ozerhalil519@gmail.com>
2024-11-26 08:52:34 +0300

    inital commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const path = require("path")
+
+const app = express()
+
+dotenv.config({path: "config.env"})
+const PORT = process.env.PORT || 8080
+
+//log request
+app.use(morgan("tiny"))
+
+
+//load routes
+app.use("/",require("./server/routes/router"))
+
+
+
+//parse request to body parser
+app.use(bodyParser.urlencoded({extended:true}))
+
+//view engine
+app.set("view engine", "ejs")
+// app.set("views",path.resolve(__dirname,"views"))
+
+//load assets
+app.use("/css", express.static(path.resolve(__dirname,"assets/css")))
+app.use("/img", express.static(path.resolve(__dirname,"assets/img")))
+app.use("/js", express.static(path.resolve(__dirname,"assets/js")))
+
+app.get("/", (req,res)=>{
+    res.render("index")
+})
+
+app.listen(PORT,()=>{console.log(`server is running on http://localhost:${PORT}`)})
+
+let users = [
+    { id: 1, email: "john@example.com", status: "active", gender: "male" },
+    { id: 2, email: "jane@example.com", status: "inactive", gender: "female" },
+  ];
