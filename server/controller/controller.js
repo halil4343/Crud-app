082369dc0ddb2314197b5c46e22ad7977ab9c69c
halil ozer <ozerhalil519@gmail.com>
2024-11-28 16:38:21 +0300

    created rest of the API (update, delete, retrieve)

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -28,19 +28,72 @@ exports.create = (req,res) =>{
         })
 }
 
-//retrieve and return a/all users
+//retrieve and return an/all user/s
 exports.find =(req,res) =>{
+    if(req.query.id){
+        const id = req.query.id
+        userDb.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({message:"Not found user with id " + id})
+                }else{
+                    res.send(data)
+                    console.log(data)
+                }
+            })
+            .catch(err=>{
+                res.status(500).send({message : err.message || "Error occured while retriving user with id " + id})
+            })
+    }else{
+        userDb.find()
+            .then(user=>{
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({message:err.message || "Error occured while retriving user info"})
+            })
 
+    }
 }
 
 //update a new idtified user by user id
 exports.update = (req,res) =>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({message:"Update data can not be empty!"})
+        }
+
+        const id = req.params.id
+        userDb.findByIdAndUpdate(id,req.body,{useFindAndModify: false})
+            .then(data=>{
+                if(!data){
+                    res.status(404).send({message: `Cannot find user with id : ${id} and update!`})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err=>{
+                res.status(500).send({message: err.message || "Error occured while updating the user data"})
+            })
 
 }
 
 //delete
 
 exports.delete = (req,res) => {
+    const id = req.params.id
 
+    userDb.findByIdAndDelete(id)
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message : `Cannot find the user with id : ${id} and delete! Maybe id is wrong.`})
+
+            }else{
+                res.send({message : "User deleted succesfully!"})
+            }
+        }).catch(err=>{
+            res.status(500).send({message : `Error occured while deleting the user data with id : ${id} !`})
+        })
 }
 
